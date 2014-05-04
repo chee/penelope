e7fac630f8cf78df625db1260f4066f81377384f
chee <chee@snaek.org>
2014-05-04 10:21:35 -0600

    git behind me satan

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Www::Application.config.secret_token = '17a626b77ed498e5cf5959b2ca0a6e543c9d5dad54ee4d0f4d2c6543ee0b9e09752462cd83cc0223ae158a51e23971ab66c4b19f7866401268578436443ccd3a'
