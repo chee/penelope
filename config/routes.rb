c9e6fd80a1b14d71c68d5ee528d0a5cd47fa71fb
chee <chee@snaek.org>
2014-05-08 23:23:40 +0100

    add the route to point to holding for now

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Www::Application.routes.draw do
-  # The priority is based upon order of creation:
-  # first created -> highest priority.
-
-  # Sample of regular route:
-  #   match 'products/:id' => 'catalog#view'
-  # Keep in mind you can assign values other than :controller and :action
+  root :to => "holding#index"
+  root :to => "slots#index"
 
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
