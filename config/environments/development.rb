b9524397cb941ec675d8e28c05e61c94ef849eb1
chee <chee@snaek.org>
2014-05-09 00:45:34 +0100

    fix rails silent fails

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -35,5 +35,4 @@ Www::Application.configure do
   # Expands the lines which load the assets
   config.assets.debug = true
 
-  config.middleware.insert_after ActionDispatch::Static, Rack::LiveReload
 end
